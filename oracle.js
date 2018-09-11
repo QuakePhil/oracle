dcdd6a87a48c95fe9dea33d7f9db9de9f40554e1
QuakePhil <phil.nyc@gmail.com>
2018-09-11 06:23:14 -0400

    Refactor

--- oracle.js
+++ oracle.js
@@ -1,4 +1,4 @@
-// todo, refactor to use {f: 99, d: 99} instead of 'ffff..ddd..'
+// todo reduce to smaller tails if not enough found (currently if not any found)
 
 // JSON.stringify(oracle.history)
 // trained first on max = 3, then 4, etc
@@ -7,10 +7,6 @@ function traindata(train) {
     return JSON.parse('[{},{},{},{"ddf":"fffffdddddfddfdddddd","dff":"fffdffffdddfdfdf","fff":"dddddffdffffdfdfd","ffd":"ddfffddddfdfddf","fdd":"ddfdfffdfdffddfdffff","ddd":"fffddddddfdffddffd","fdf":"dddddddfddddddfdfdddddfdfdddfdddddfddddfdd","dfd":"fdffffffffdffdffffdffdfffffffffffdffddfdfdffffd","dddd":"fddddfddddddfdddddfdfdf","dddf":"fffffffdddd","ddff":"fddfffdf","dfff":"dffffdffd","fffd":"dddddfff","ffdd":"fddddf","fddf":"fddddddd","dffd":"dfffffffffffff","fddd":"dddddfffff","ffdf":"dfddddddffdffdfd","fdfd":"dfddffffffffffffffdffdfffffdfd","dfdd":"ddffdfdfdfdf","ffff":"ffffffdddffdfdd","fdff":"dddddddddffddf","dfdf":"dfdddffddfddddfdddfddddfdddf","ddfd":"fdfdfddfdf","dfffd":"dddddddf","fffdd":"dddddddddfdddd","ffddd":"dddddffffdddddf","fdddd":"fddffddffff","ddddf":"fffffffffdd","dddff":"ffffffffdfffdff","ddfff":"fffddddddffffd","dffff":"dddffdffdd","ffffd":"ddddddfffd","ddddd":"ffff","fdddf":"ffffff","ddffd":"df","dffdd":"dd","ffddf":"f","fddff":"f","fffff":"ddffdfd","dffdf":"d","ffdfd":"fffff","fdfdf":"dddfddfdddddddddddddfddd","dfdfd":"dffdffffffffffffffffff","fdfdd":"dd","dfddd":"fd","fffdf":"dddd","dfdff":"fdff","fdfff":"fff","dddfd":"ff","ddfdf":"fd","fdffd":"d"},{"dddd":"fddddfddddddfdddddfdfdf","dddf":"fffffffdddd","ddff":"fddfffdf","dfff":"dffffdffd","fffd":"dddddfff","ffdd":"fddddf","fddf":"fddddddd","dffd":"dfffffffffffff","fddd":"dddddfffff","ffdf":"dfddddddffdffdfd","fdfd":"dfddffffffffffffffdffdfffffdfd","dfdd":"ddffdfdfdfdf","ffff":"ffffffdddffdfdd","fdff":"dddddddddffddf","dfdf":"dfdddffddfddddfdddfddddfdddf","ddfd":"fdfdfddfdf","dfffd":"dddddddf","fffdd":"dddddddddfdddd","ffddd":"dddddffffdddddf","fdddd":"fddffddffff","ddddf":"fffffffffdd","dddff":"ffffffffdfffdff","ddfff":"fffddddddffffd","dffff":"dddffdffdd","ffffd":"ddddddfffd","ddddd":"ffff","fdddf":"ffffff","ddffd":"df","dffdd":"dd","ffddf":"f","fddff":"f","fffff":"ddffdfd","dffdf":"d","ffdfd":"fffff","fdfdf":"dddfddfdddddddddddddfddd","dfdfd":"dffdffffffffffffffffff","fdfdd":"dd","dfddd":"fd","fffdf":"dddd","dfdff":"fdff","fdfff":"fff","dddfd":"ff","ddfdf":"fd","fdffd":"d"},{"dfffd":"dddddddf","fffdd":"dddddddddfdddd","ffddd":"dddddffffdddddf","fdddd":"fddffddffff","ddddf":"fffffffffdd","dddff":"ffffffffdfffdff","ddfff":"fffddddddffffd","dffff":"dddffdffdd","ffffd":"ddddddfffd","ddddd":"ffff","fdddf":"ffffff","ddffd":"df","dffdd":"dd","ffddf":"f","fddff":"f","fffff":"ddffdfd","dffdf":"d","ffdfd":"fffff","fdfdf":"dddfddfdddddddddddddfddd","dfdfd":"dffdffffffffffffffffff","fdfdd":"dd","dfddd":"fd","fffdf":"dddd","dfdff":"fdff","fdfff":"fff","dddfd":"ff","ddfdf":"fd","fdffd":"d"},{}]')
   }
   return []
-}
-
-function randomchar(string) {
-  return string[Math.floor(Math.random() * string.length)]
 }
 
 var Oracle = function(train) {
@@ -32,7 +28,7 @@ Oracle.prototype.predict = function() {
     let partial = this.tail.substring(this.tailmax - i, this.tailmax)
     remember = this.history[i][partial]
     if (remember !== undefined) {
-      console.log('Remembered ' + remember + ' for ' + partial)
+      console.log('Remembered ' + JSON.stringify(remember) + ' for ' + partial)
       //console.log(partial, this.history[i], this.history[i][partial])
       break
     }
@@ -40,9 +36,10 @@ Oracle.prototype.predict = function() {
 
   // default to 50/50 if nothing remembered
   if (remember === undefined) {
-    remember = 'df'
+    let guess = Math.floor(Math.random() * 2)
+    return guess == 0 ? 'd' : 'f'
   }
-  return randomchar(remember)
+  return remember.f > remember.d ? 'f' : 'd'
 }
 
 Oracle.prototype.remember = function(observed) {
@@ -50,9 +47,9 @@ Oracle.prototype.remember = function(observed) {
   for (let i = this.tailmax; i >= 3; i--) {
     if (this.tail.length >= i) {
       if (this.history[i][this.tail] === undefined) {
-        this.history[i][this.tail] = ''
+        this.history[i][this.tail] = {f: 0, d: 0}
       }
-      this.history[i][this.tail] += observed // TODO: sort|uniq or gcd this value to save memory
+      this.history[i][this.tail][observed]++
     }
   }
 //  console.log(this.history[i])
